15349e7581a997a5ce1042ce02ab9ceb0e6e53fc
AHMED HAFDI <58536023+HAFDIAHMED@users.noreply.github.com>
2023-12-29 14:21:48 +0100

    resolve issues
    resolve issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { ProduitComponent } from './app/composants/produit/produit.component';
+import { CommonModule } from '@angular/common';
 // Import components and services
 
 @NgModule({
  declarations: [
  // Declare your components here
- AppComponent
  ],
  imports: [
- // Import Angular modules here
- BrowserModule,
- ProduitComponent
+    CommonModule,
+     BrowserModule,
+ ProduitComponent,
+ AppComponent
  ],
  providers: [],
- bootstrap: [AppComponent]
+ bootstrap: []
 })
 export class AppModule { }
